9b23dd0e24911d6b172ef8d5d637cf7c61db0449
Владимир Чигин <justlikevova@gmail.com>
2025-04-27 19:13:05 +0300

    monitoring

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 from flask import Flask
+import logging
+from prometheus_flask_exporter import PrometheusMetrics
+import logging_loki
 
 
+handler = logging_loki.LokiHandler(
+    url="http://loki:3100/loki/api/v1/push", 
+    tags={"application": "my-app"},
+    version="1",
+)
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+logging.basicConfig(filename='app.log', level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.addHandler(handler)
 
 @app.route('/')
 def home():
-    return 'Hello, Docker CI/CD!'
-    
+    logger.info('Received request on home page')
+    return 'Hello, Monitoring!'
+
+@app.route('/error')
+def error():
+    logger.error('Simulated error occurred')
+    return 'Error simulated', 500
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
